886129d864ccfb8a1debf5e9f73be235041296fd
valdemir08 <valdemireverton08@gmail.com>
2022-02-01 12:07:30 -0300

    first commit

---
+++ dominando_ides_java/primeiroProgramaJava/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		
+		System.out.println(gato);
+		System.out.println(livro);
+//		int a = 1, b = 2;
+//		System.out.println("Hello World! "+ (a+b));
+	}
+	
+	
+
+}
+
+class Livro{
+	private String nome;
+	private String npag;
+	
+	
+}
