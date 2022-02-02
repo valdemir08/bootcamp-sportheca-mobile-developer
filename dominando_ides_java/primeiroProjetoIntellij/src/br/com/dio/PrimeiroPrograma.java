8c0dc36c53d84a83acd130eaf14e84fe3e245a51
valdemir08 <valdemireverton08@gmail.com>
2022-02-02 08:52:29 -0300

    correção final

--- dominando_ides_java/primeiroProjetoIntellij/src/br/com/dio/PrimeiroPrograma.java
+++ dominando_ides_java/primeiroProjetoIntellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,7 +8,6 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
-
         Livro livro = new Livro("O problema dos 3 corpos", 300);
         System.out.println(livro);
 
